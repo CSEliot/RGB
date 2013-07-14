873b7928622cb03bfda216db70418d2b25b8b155
ecsit <ecsit@localhost>
2013-07-14 17:50:43 -0400

    Even small commits will have a changelog update now: ------------------------- beta v1.22b // 7.14.2013 ------------------------- Added: 	- YAY we have a credits.txt now! Less illegal! 	- Toggle controls by pressing K to ASD/RGB Changed: 	- Game is automatically in debug mode, which is finished. 	  This includes the following: 	  	- Every time the game is run, data is written to a log file. 	  	- Hitting escape won't bring up a pause menu, but P will. 	- Game will not be in fullscreen, for now. 	- Circles fade when matched. 	- StarBackGround Rotation speed lowered. 	- *MAJOR GAMEPLAY CHANGES* 		- Circles will always hit the ring on time. 		- The longer you take to match the circle's 			color, the less points you'll gain. 		- If matched, circles will fade out. Removed: 	- Herobrine Fixed: -- Known-Issues: 	- Circles aren't in time correctly past 3 	seconds. *NOTE: Everything Star related will be forgotten for 	   now. The focus is on getting circles feel 	   right.

--- src/main/game/log.py
+++ src/main/game/log.py
@@ -12,27 +12,27 @@ def log(c):
     #===========================================================================
     saveDir = os.path.join(c.DATA_DIR, 'log.txt')
     # --TESTING the full-screen function//--
-    for i in range(2):  # @UnusedVariable
-        try:
-            # toggle_fullscreen returns a 1/0 based on if it worked
-            attempt_fullscreen = pygame.display.toggle_fullscreen()
-            scn_tst = "About the full-screen, there were no errors boss!"
-        except Exception as e:
-            attempt_fullscreen = "Fullscreen Failed"
-            scn_tst = "Full-screen error({0}): {1}".format(e.errno, e.strerror)
+    #     for i in range(2):  # @UnusedVariable
+    #         try:
+    #             # toggle_fullscreen returns a 1/0 based on if it worked
+    #             attempt_fullscreen = pygame.display.toggle_fullscreen()
+    #             scn_tst = "About the full-screen, there were no errors boss!"
+    #         except Exception as e:
+    #             attempt_fullscreen = "Fullscreen Failed"
+    #             scn_tst = "Full-screen error({0}): {1}".format(e.errno, e.strerror)
     # --LOGGING
     # if the original location to print out to is the same as current.
     # meaning, if it is still printing to the screen.
     if c.OG_STDOUT == sys.stdout:
         # create a file to print to.
-        logFile = open(saveDir, 'a')
+        logFile = open(saveDir, 'w')
         # set the sys.stdout equal to the file we're writing to.
         sys.stdout = logFile
         debug(c.DEBUG, "LOGGING TO FILE BEGINNING--")
         debug(c.DEBUG, c.DATE)
         debug(c.DEBUG, "Display Info: {0}, {1}".format(c.whichDisplay, c.screenError))
-        debug(c.DEBUG, (attempt_fullscreen, scn_tst))
-        return logFile
+
+
     else:
         debug(c.DEBUG, "LOGGING TO FILE ENDING--")
         sys.stdout = c.OG_STDOUT
