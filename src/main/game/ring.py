1bea067476717109f9462af63861b438e7b4d6e3
ecsit <ecsit@localhost>
2013-07-30 02:08:48 -0400

    beta v1.3b // 7.30.2013 ------------------------------------------------- Added: Changed: Removed: Fixed: 	- ring's color changing glow is off-center. 	- Ring's glow goes way off in diagonal angles. Known-Issues: 	- Circles are growing too quickly. Will probably switch from using a resizing 		algorithm to simply using animated circles. Most likely an implementation for 		1.4 Intentional:

--- src/main/game/ring.py
+++ src/main/game/ring.py
@@ -7,18 +7,18 @@ class Ring(pygame.sprite.Sprite):
     def __init__(self, CENTER, image, glow):
         pygame.sprite.Sprite.__init__(self)
         self.image, self.rect = image, image.get_rect()
-        self.glow = glow
-        self.glow_center = (CENTER[0]-58, CENTER[1]-8)
+        self.glow, self.glow_rect = glow, glow.get_rect()
+        self.glow_center = (CENTER[0]-63, CENTER[1]-10)
+        self.glow_rect.center = self.glow_center
         self.OGImage = self.image.copy()
-        self.OGGlow = self.glow.copy()
         self.OGCenter = CENTER
-        pgext.color.setColor(self.OGGlow, (0,0,0))
+        pgext.color.setColor(self.glow, (0,0,0))
         self.rect.center = self.OGCenter
         self.MAX_COLLIDE_SIZE = 290
         self.MIN_COLLIDE_SIZE = 280
         self.radius = 300
         self.angle = 0.0
-        self.rotate_by = 0.0
+        self.rotate_by = 10
         self.rotation_speed = 5
 
     def spin(self, direction):  # Will change ring angle, based on direction
@@ -41,14 +41,14 @@ class Ring(pygame.sprite.Sprite):
             self.angle = 270
 
     def update(self):
-        # self.angle += self.rotate_by
-        self.image = pygame.transform.rotozoom(self.OGImage, self.angle, 1)
-        self.glow = pygame.transform.rotozoom(self.OGGlow, self.angle, 1)
+        tempImage = self.OGImage.copy()
+        tempImage.blit(self.glow, self.glow_rect)
+        self.image = pygame.transform.rotozoom(tempImage, self.angle, 1)
+#         self.glow = pygame.transform.rotozoom(self.OGGlow, self.angle, 1)
         self.rect = self.image.get_rect(center=(self.OGCenter))
-        self.glow_rect = self.glow.get_rect(center = (self.glow_center))
-        self.image.blit(self.glow, self.glow_rect)
+#         self.glow_rect = self.glow.get_rect()
+#         self.glow_rect.center = self.glow_center
         
     def glowColor(self, color):
-        print "TETSING CGLOW"
-        pgext.color.setColor(self.OGGlow, color)
+        pgext.color.setColor(self.glow, color)
 
